a32d0abb6397b0269dbe68936654c4f51796da6e
taylor marlin <taylmarl@umich.edu>
2021-03-14 12:06:38 -0400

    Committing for the second edit of this python file.

--- hw5_git.py
+++ hw5_git.py
@@ -1,1 +1,3 @@
+# This is the second line I am adding to this file. 
+# I want to test that I am pushing an edit to the github branch accordingly :)
 print("I hope I am using git bash correctly... TM")
